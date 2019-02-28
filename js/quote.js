29bfcc68ee6c5857be05f24f1df4215da0029d84
Ethan Miller <ethdmill@gmail.com>
2019-02-27 19:50:28 -0500

    add second html and random quote js

---
+++ js/quote.js
@@ -0,0 +1,18 @@
+var firstQuote = document.getElementById('firstQuote');
+var quoteArray = [
+  "Quote 1",
+  "Quote 2",
+  "Quote 3",
+  "Quote 4",
+  "Quote 5",
+  "Quote 6",
+  "Quote 7",
+  "Quote 8",
+  "Quote 9",
+  "Quote 10",
+];
+
+function generateQuote () {
+  var randomQuote = quoteArray[Math.round(Math.random() * (quoteArray.length - 1))];
+  firstQuote.innerHTML = randomQuote;
+}
