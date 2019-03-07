264aff4b0860944a79c3b3d24769f9fa155ae42b
Ethan Miller <ethdmill@gmail.com>
2019-03-06 19:57:05 -0500

    changed html, began to add css, added new javascript

---
+++ js/new_list.js
@@ -0,0 +1,10 @@
+//this is the button that creates a new list item on the page
+
+const newListItem = document.createElement("li");
+
+
+function createNewList () {
+  doucment.createElement
+
+  newListItem.className = "new-list-item";
+}
--- js/quote.js
+++ js/quote.js
@@ -1,5 +1,9 @@
+//These two variables select the firstQuote element <p> and then
+//create a something to store a later HTML change, respectively
 var firstQuote = document.getElementById('firstQuote');
 var randomQuote;
+
+//This is an array full of quotes for the later function to choose from
 var quoteArray = [
   "Quote 1",
   "Quote 2",
@@ -13,7 +17,13 @@ var quoteArray = [
   "Quote 10",
 ];
 
+//This function selects a random item from quoteArray and displays it by
+//changing the HTML content of the <p> element marked as "firstQuote"
 function generateQuote () {
   randomQuote = quoteArray[Math.round(Math.random() * (quoteArray.length - 1))];
   firstQuote.innerHTML = randomQuote;
 }
+
+//I am planning to add functionality so that the initial "Quote 0"
+//is also selected from the array at random, but for now, it is
+//the same every time on page load
