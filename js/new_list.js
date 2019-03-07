1071f060bfc8cf4539d0496ed833a19b86a444bb
Ethan Miller <ethdmill@gmail.com>
2019-03-07 14:39:47 -0500

    continued work on new js list add function, added test html

--- js/new_list.js
+++ js/new_list.js
@@ -1,10 +1,15 @@
 //this is the button that creates a new list item on the page
 
-const newListItem = document.createElement("li");
-
+const ul = document.getElementById("list-items");
+const input = document.getElementById("title-input");
+const newItem;
 
 function createNewList () {
-  doucment.createElement
-
-  newListItem.className = "new-list-item";
+  newItem = document.createElement("li");
+  var listTitle = input.value;
+  newItem.setAttribute("id", listTitle);
+  newItem.appendChild(document.createTextNode(listTitle));
+  ul.appendChild(newItem);
 }
+
+//fix this functionality...something is wrong
